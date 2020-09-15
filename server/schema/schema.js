d88991f5b78a9fe5a94f6ada1a1bd54dad7ed8f0
Albin Jose <albinavirappatt@gmail.com>
2020-09-15 08:38:26 +0530

    create contest schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -30,3 +30,40 @@ const UserType = new GraphQLObjectType({
         }
     })
 })
+
+const ContestType = new GraphQLObjectType({
+    name:'Contest',
+    fields:() => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        tagline: { type: GraphQLString },
+        createdBy: {
+            type: UserType,
+            resolve(parent, args){
+                //fetch user details of the user
+            }
+        },
+        maxParticipants: { type: GraphQLInt },
+        currentParticipants: { type: GraphQLInt },
+        enrty:{
+            type: new GraphQLList(ContestEntryType),
+            resolve(parent, args){
+                // fetch entry details
+            }
+        }
+    })
+})
+
+const ContestEntryType = new GraphQLObjectType({
+    name:'ContestEntry',
+    fields:() => ({
+        url: { type: GraphQLString },
+        participant: {
+            type: UserType,
+            resolve(parent,args){
+                // fetch contestant details
+            }
+        },
+        like: { type: GraphQLInt },
+    })
+})
