39c3ad0db52b00a5de154ee1f5916ffab45037a9
Albin Jose <albinavirappatt@gmail.com>
2020-09-26 22:22:21 +0530

    refactor(uploadImage): change upload image function from using loacal file to stream

--- server/resolver_handlers/UploadImageHandler.js
+++ server/resolver_handlers/UploadImageHandler.js
@@ -11,39 +11,46 @@ const uploadImageFunc = async function(args,{req, res}) {
       return args.file.then ( file => {
         const {createReadStream, filename, mimetype} = file
 
+        let streamSize = parseInt(req.headers['content-length'])
+
         const fileStream = createReadStream()
 
         const newFilename = uuidv4()
         
         // write stream to temp file
-        fileStream.pipe(fs.createWriteStream(__dirname+`/../temp/uploadedFiles/${filename}`))
+        // fileStream.pipe(fs.createWriteStream(__dirname+`/../temp/uploadedFiles/${filename}`))
           
-        blobService.createAppendBlobFromLocalFile('contest-entry-container',`${args.contestId}/${newFilename}`,__dirname+`/../temp/uploadedFiles/${filename}`,(error,response) => {
-          if(!error){
-            console.log(response)
-            let entry = {
-              url:`https://contestappstorageaccount.blob.core.windows.net/contest-entry-container/${args.contestId}/${newFilename}`,
-              participant: req.userId
-            }
-            contestEntryModel.findOneAndUpdate({contestId: args.contestId},{$push:{entries:entry}},(err, doc) => {
-              console.log(doc)
-            })
+        // blobService.createAppendBlobFromLocalFile('contest-entry-container',`${args.contestId}/${newFilename}`,__dirname+`/../temp/uploadedFiles/${filename}`,(error,response) => {
+        //   if(!error){
+        //     let entry = {
+        //       url:`https://contestappstorageaccount.blob.core.windows.net/contest-entry-container/${args.contestId}/${newFilename}`,
+        //       participant: req.userId
+        //     }
+        //     contestEntryModel.findOneAndUpdate({contestId: args.contestId},{$push:{entries:entry}},(err, doc) => {
+        //       console.log("updated")
+        //     })
               
-          }
+        //   }
 
-          // delete tem file after upload
-          fs.unlink(__dirname+`/../temp/uploadedFiles/${filename}`,(err) => {
-            if(err){
-              console.log(err)
-            }
-          })
-        })
+        //   // delete tem file after upload
+        //   fs.unlink(__dirname+`/../temp/uploadedFiles/${filename}`,(err) => {
+        //     if(err){
+        //       console.log(err)
+        //     }
+        //   })
+        // })
         
-      // blobService.createBlockBlobFromStream('contest-entry-container',`contest/${filename}`,fileStream,50*1024*1024,(error,response) => {
-      //   if(!error){
-      //     console.log(response)
-      //   }
-      // })
+      blobService.createBlockBlobFromStream('contest-entry-container',`${args.contestId}/${newFilename}`,fileStream,streamSize,(error,response) => {
+        if(!error){
+          let entry = {
+            url:`https://contestappstorageaccount.blob.core.windows.net/contest-entry-container/${args.contestId}/${newFilename}`,
+            participant: req.userId
+          }
+          contestEntryModel.findOneAndUpdate({contestId: args.contestId},{$push:{entries:entry}},(err, doc) => {
+            console.log("updated")
+          })
+        }
+      })
 
 
         return true;
