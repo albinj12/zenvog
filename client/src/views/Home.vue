a08afc34807897e3a57ac4c152e6615120dea46e
Albin Jose <albinavirappatt@gmail.com>
2020-10-07 18:55:25 +0530

    feat(homepage):create basic home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <v-container fluid>
+      <v-row justify="center">
+        <v-col cols=5 class="my-16 pt-16">
+          <v-card color="grey lighten-4" class="d-flex flex-column align-center">
+            <h3 class="pa-10">Conduct an online Photography Competetion? ContestApp made it easy for you.</h3>
+            <v-btn width=250 class="my-10">Create</v-btn>
+          </v-card>
+        </v-col>
+        <v-col cols=5 class="my-16 pt-16">
+          <v-card color="grey lighten-4" class="d-flex flex-column align-center">
+            <h3 class="pa-10">Are you a photographer? Participate in various online Competetions happeining around the globe</h3>
+            <v-btn width=250 class="my-10">Participate</v-btn>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols=10>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident commodi, quia iure ratione numquam veniam eaque quaerat reprehenderit. Placeat sequi nihil laudantium! Vero aspernatur maxime inventore, quis obcaecati excepturi molestiae?Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ea blanditiis nesciunt. Eveniet, quasi iusto tenetur quidem tempora nisi nihil, officiis nulla pariatur voluptatum odio ducimus asperiores modi deleniti explicabo?</p>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
